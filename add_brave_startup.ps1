2171869e832e47e3bef643e17072cdfab4ab8f07
Littlellama <matthewroylange@gmail.com>
2026-05-31 22:04:37 -0700

    cleanup: remove personal one-shot setup scripts

--- add_brave_startup.ps1
+++
@@ -1,4 +0,0 @@
-$bravePath = "$env:LOCALAPPDATA\BraveSoftware\Brave-Browser\Application\brave.exe"
-$value = "`"$bravePath`" --remote-debugging-port=9222 --no-startup-window"
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "BraveBrowser" -Value $value
-Get-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" | Select-Object RuneSync, BraveBrowser
--- mkshortcut.ps1
+++
@@ -1,9 +0,0 @@
-$ws = New-Object -ComObject WScript.Shell
-$s = $ws.CreateShortcut("C:\Users\Matth\OneDrive\Desktop\RuneSync.lnk")
-$s.TargetPath = "pythonw"
-$s.Arguments = '"C:\Users\Matth\RuneSync\watcher.py"'
-$s.WorkingDirectory = "C:\Users\Matth\RuneSync"
-$s.WindowStyle = 7
-$s.Description = "RuneSync - Auto Rune Importer"
-$s.Save()
-echo "Shortcut updated"
