22310b7e0eb72624948fc87718fdd3bcb7f39b82
Littlellama <matthewroylange@gmail.com>
2026-05-31 22:04:37 -0700

    cleanup: remove personal one-shot setup scripts

--- fix_startup.ps1
+++
@@ -1,4 +0,0 @@
-$watcherExe = "C:\Users\Matth\RuneSync\dist\RuneSyncWatcher.exe"
-Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "RuneSync" -Value "`"$watcherExe`""
-$val = (Get-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run").RuneSync
-echo "RuneSync startup is now: $val"
--- update_startup.ps1
+++
@@ -1,4 +0,0 @@
-$py = 'C:\Users\Matth\AppData\Local\Programs\Python\Python314\python.exe'
-$script = 'C:\Users\Matth\game_watcher.py'
-Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'RuneSync' -Value "`"$py`" `"$script`""
-Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' | Select-Object RuneSync | Format-List
